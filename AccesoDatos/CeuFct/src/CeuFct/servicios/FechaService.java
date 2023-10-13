8fc28d46879a971e0da0ee752e1eaaf6ad9185f6
Gonzalo Conejo <gonzaloconejogomez@gmail.com>
2023-10-13 17:24:57 +0200

    d

---
+++ AccesoDatos/CeuFct/src/CeuFct/servicios/FechaService.java
@@ -0,0 +1,14 @@
+package CeuFct.servicios;
+
+import java.sql.Connection;
+
+import CeuFct.modelo.Fecha;
+import CeuFct.modelo.Registro;
+
+public class FechaService {
+	
+	//Consulta y devuelve una lista de fechas
+	public List<Fecha> consultarFecha() {
+		return null;
+	}
+}
